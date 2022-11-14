6453cdc48a6ab17f30eb9adf3cb1b1e49ba5a695
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-11-14 00:41:24 -0500

    Further work on affiliations menu, rating algorithm

--- src/components/character_creator/vtm/5eClanSelect.vue
+++ src/components/character_creator/vtm/5eClanSelect.vue
@@ -9,8 +9,14 @@
       </q-header>
 
       <q-page-container>
-        <q-page padding class="bg-primary">
-          <q-stepper v-model="step" vertical color="primary" animated>
+        <q-page padding class="bg-dark">
+          <q-stepper
+            v-model="step"
+            vertical
+            color="primary"
+            animated
+            class="stepper"
+          >
             <q-step
               :name="1"
               title="Select a clan"
@@ -26,11 +32,54 @@
                 style="margin-bottom: 20px"
                 class="select"
                 label-color="primary"
+                @update:model-value="clanSelected"
               />
               <q-separator />
-              For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.
+              <div>Clan Description: {{ clanDesc }}</div>
+              <q-separator />
+              <div class="q-mt-md q-mb-sm">
+                Disciplines: (Choose two in-clan Disciplines. Put two dots in
+                one and one dot in the other.)
+              </div>
+              <q-item v-for="(disciplines, key) in clanDisciplines" :key="key">
+                <q-item>
+                  {{ disciplines }}:
+                  <q-tooltip
+                    anchor="top right"
+                    self="center left"
+                    :offset="[10, 10]"
+                    class="bg-dark text-body2"
+                  >
+                    {{ discExplained[key] }}
+                  </q-tooltip>
+                </q-item>
+                <q-rating
+                  size="1.5em"
+                  icon="thumb_up"
+                  :max="2"
+                  v-model="discChoices[key]"
+                  @update:model-value="discSelected()"
+                />
+              </q-item>
+              <q-separator />
+              Bane: {{ clanBane }}
+              <q-separator />
+              <q-input
+                filled
+                bg-color="grey-3"
+                v-model="sire"
+                label="Your sire's name, if you know *"
+                autogrow
+                lazy-rules
+                style="margin-top: 15px"
+                label-color="primary"
+                :rules="[
+                  (val) =>
+                    val === null ||
+                    val.length <= 2000 ||
+                    'Please keep this field under 2000 characters',
+                ]"
+              />
 
               <q-stepper-navigation>
                 <q-btn @click="step = 2" color="primary" label="Continue" />
@@ -59,7 +108,7 @@
               </q-stepper-navigation>
             </q-step>
 
-            <q-step :name="3" title="Ad template" icon="assignment" disable>
+            <q-step :name="3" title="Ad template" icon="assignment">
               This step won't show up because it is disabled.
             </q-step>
 
@@ -105,6 +154,26 @@
   </q-dialog>
 </template>
 
+<style>
+.q-stepper__dot {
+  color: #e31c25;
+}
+.q-stepper__line {
+  color: #e31c25;
+}
+
+.stepper {
+  background-color: #222831;
+
+  font-size: larger;
+}
+@media only screen and (max-width: 600px) {
+  .stepper {
+    font-size: medium;
+  }
+}
+</style>
+
 <script>
 import { defineComponent } from "vue";
 import { ref, computed } from "vue";
@@ -120,33 +189,69 @@ export default defineComponent({
     const clan = ref(props.info.clan);
     const sect = ref(props.info.sect);
     const archtype = ref(props.info.archtype);
+    const sire = ref(null);
+    const clanDesc = ref(
+      "The 'Rabble' rebel against power and rage against tyranny."
+    );
+    const clanDisciplines = ref(["Celerity", "Potence", "Presence"]);
+    const discExplained = ref([
+      "Supernatural quickness and reflexes",
+      "The Discipline of physical vigor and strength",
+      "The ability to attract, sway, and control emotions",
+    ]);
+    const clanBane = ref(
+      "Violent Temper: Subtract dice equal to the Bane Severity of the Brujah from any roll to resist fury frenzy. This cannot take the pool below one die"
+    );
+
+    const discChoices = ref([0, 0, 0]);
+
+    const discChoicesRemaining = ref(3);
+    const disableRating = ref(false);
 
     return {
       clan,
+      clanBane,
+      clanDesc,
+      clanDisciplines,
+      disableRating,
+      discChoices,
+      discChoicesRemaining,
+      discExplained,
       sect,
+      sire,
       archtype,
       step: ref(1),
       dialogRef,
       onDialogHide,
-      test: ref("kfhnweiuohfihkwjebfwe"),
 
       clanOptions: [
+        "Banu Haqim",
         "Brujah",
+        "Caitiff",
         "Gangrel",
+        "Hecata",
+        "Lasombra",
         "Malkavian",
         "Nosferatu",
         "Toreador",
         "Tremere",
+        "Tzimisce",
         "Ventrue",
-        "Caitiff",
+        "The Ministry",
         "Thin-Blood",
       ],
-      sectOptions: ["Camarilla", "Anarchs", "Sabbat"],
+      sectOptions: [
+        "Anarchs",
+        "Camarilla",
+        "Independent",
+        "Sabbat",
+        "Clanless",
+      ],
 
       archtypeOptions: ["Murderhobo", "Hobo"],
 
       onOKClick() {
-        onDialogOK(clan);
+        onDialogOK({ clan: clan });
       },
 
       // we can passthrough onDialogCancel directly
@@ -157,6 +262,52 @@ export default defineComponent({
     return {
       layout: false,
     };
+  },
+  methods: {
+    clanSelected() {
+      console.log(this.clan);
+      switch (this.clan) {
+        case "Malkavian":
+          this.clanDesc =
+            "The madness of the 'Lunatics' conceals and reveals truths.";
+          this.clanDisciplines = ["Auspex", "Dominate", "Obfuscate"];
+          this.discExplained = [
+            "Extrasensory perception, awareness, and premonitions",
+            "Mind control practiced through one’s piercing gaze",
+            "The ability to remain obscure and unseen, even in crowds",
+          ];
+          break;
+        case y:
+          // code block
+          break;
+        default:
+          this.clanDesc =
+            "The 'Rabble' rebel against power and rage against tyranny.";
+      }
+    },
+    discSelected() {
+      let sum = 0;
+
+      this.discChoices.forEach((choice) => (sum += choice));
+
+      function arrayEquals(a, b) {
+        return (
+          Array.isArray(a) &&
+          Array.isArray(b) &&
+          a.length === b.length &&
+          a.every((val, index) => val === b[index])
+        );
+      }
+
+      if (sum > 3 || arrayEquals([1, 1, 1], this.discChoices)) {
+        this.discChoices = [0, 0, 0];
+        this.$q.notify({
+          type: "negative",
+          textColor: "white",
+          message: "Please re-read the discipline instructions.",
+        });
+      }
+    },
   },
 });
 </script>

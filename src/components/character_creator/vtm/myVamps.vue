0cd065223ae33b0bc95b85eb870c65eaa496cd57
Chase Murtaugh <Chasemurtaugh@gmail.com>
2022-12-13 19:15:15 -0500

    REEEEEEEEEEE

--- src/components/character_creator/vtm/myVamps.vue
+++ src/components/character_creator/vtm/myVamps.vue
@@ -204,7 +204,7 @@ export default {
 
   data() {
     return {
-      kindred: [],
+      kindred: null,
     };
   },
 
@@ -220,11 +220,14 @@ export default {
     this.$q.loading.show({
       delay: 450, // ms
     });
+
+    console.log("kindred id " + kindredId);
     this.kindred = await this.$axios
       .get(baseUrl + "/vampires/myVampire/" + kindredId, {
         withCredentials: true,
       })
       .then((resp) => {
+        console.log(resp);
         this.$q.loading.hide();
         return resp.data;
       })

2fca135425f750926650938e31b29ffb9ab94346
Daelso <11338807+Daelso@users.noreply.github.com>
2023-02-01 23:26:58 -0500

    Centers login/register

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,11 +18,11 @@
           <router-link to="/" class="titleLogo">SchreckNet</router-link>
         </q-toolbar-title>
 
-        <q-item v-if="!logInCheck" clickable>
+        <q-item v-if="!logInCheck" clickable style="align-items: center">
           <router-link to="/login">Login</router-link>
         </q-item>
 
-        <q-item v-if="!logInCheck" clickable>
+        <q-item v-if="!logInCheck" clickable style="align-items: center">
           <router-link to="/register">Register</router-link>
         </q-item>
 

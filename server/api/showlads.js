6deda8276a2dde3b706df16a4f3636501ec050b3
Daelso <11338807+Daelso@users.noreply.github.com>
2023-10-08 21:35:50 -0400

    CORS update

--- server/api/showlads.js
+++ server/api/showlads.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const sequelize = require("../database");
+
 require("dotenv").config();
 const app = express();
 

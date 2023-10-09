94150bfe5917d2dc4bcbda9656719ca0fbea0437
Daelso <11338807+Daelso@users.noreply.github.com>
2023-10-09 01:27:08 -0400

    text update

--- server/api/showlads.js
+++ server/api/showlads.js
@@ -13,14 +13,29 @@ const Showlads = require("../models/lifeweb/Showlads.js");
 const lib = require("../lib");
 
 //Route is base/showlads/
-router.route("/ckey/:ckey").get(async (req, res) => {
+
+router.get("/ckeys", lib.getLimiter, async (req, res) => {
+  try {
+    const result = await sequelize.sequelize.query(
+      "SELECT DISTINCT ckey FROM showlads ORDER by ckey asc"
+    );
+    const ckeys = result[0].map((item) => item.ckey);
+
+    res.status(200).json(ckeys);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.route("/ckey/:ckey").get(lib.getLimiter, async (req, res) => {
   try {
     const ckey_stats = await Showlads.findAll({
       where: {
         ckey: req.params.ckey,
       },
     });
-    res.send(ckey_stats.dataValues);
+    res.send(ckey_stats);
   } catch (err) {
     res.status(404).send(err);
   }

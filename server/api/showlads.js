c8f839e1e1e172fd37d2e888fcbcf97f6ba5175a
Chase Murtaugh <chasemurtaugh@gmail.com>
2023-10-10 17:27:15 -0400

    showlads update

--- server/api/showlads.js
+++ server/api/showlads.js
@@ -41,4 +41,40 @@ router.route("/ckey/:ckey").get(lib.getLimiter, async (req, res) => {
   }
 });
 
+router.route("/fun_facts").get(lib.getLimiter, async (req, res) => {
+  try {
+    const [result1, result2, result3, result4, result5] = await Promise.all([
+      sequelize.sequelize.query(
+        "SELECT role, COUNT(*) AS role_count FROM showlads WHERE ckey = 'Randy Sandy' AND role != 'Unknown' GROUP BY role ORDER BY role_count DESC LIMIT 1;"
+      ),
+      sequelize.sequelize.query(
+        "SELECT ckey, COUNT(*) AS role_count FROM showlads WHERE role = 'Bum' GROUP BY ckey ORDER BY role_count DESC LIMIT 1;"
+      ),
+      sequelize.sequelize.query(
+        "SELECT role, COUNT(*) AS role_count FROM showlads GROUP BY role HAVING role_count > 40 ORDER BY role_count ASC LIMIT 1;"
+      ),
+      sequelize.sequelize.query(
+        "SELECT character_name, COUNT(*) AS played_count FROM showlads GROUP BY character_name ORDER BY COUNT(*) DESC LIMIT 1;"
+      ),
+      sequelize.sequelize.query(
+        "SELECT showlads.role, COUNT(*) AS count_played FROM lifeweb_roles AS roles INNER JOIN showlads ON showlads.role = roles.role WHERE roles.migrant_role = 1 GROUP BY showlads.role ORDER BY count_played DESC LIMIT 1; "
+      ),
+    ]);
+
+    //idk why tf these are getting duplicated some dumb node stuff
+    res
+      .status(200)
+      .json([
+        result1[0][0],
+        result2[0][0],
+        result3[0][0],
+        result4[0][0],
+        result5[0][0],
+      ]);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router; //Exports our routes

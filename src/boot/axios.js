04c1a32fdebd30e3f38510bd5ff406edebee1f44
Daelso <11338807+Daelso@users.noreply.github.com>
2024-08-29 11:05:51 -0400

    sving prog

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,12 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "https://api.example.com" });
+
+const base = window.location.href.includes("localhost")
+  ? "http://localhost:5000"
+  : window.location.origin;
+
+const api = axios.create({ baseURL: base, withCredentials: true });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api

f3b71ee3d0f757d48aff794769c4e7da38a7f958
Daelso <11338807+Daelso@users.noreply.github.com>
2024-12-09 11:43:43 -0500

    starter

---
+++ server/models/game_finder/games.js
@@ -0,0 +1,62 @@
+const Sequelize = require("sequelize");
+const db = require("../../database");
+const GameStyles = require("./game_styles");
+
+const Games = db.sequelize.define(
+  "games",
+  {
+    game_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    game_title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    game_style: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: GameStyles,
+        key: "style_id",
+      },
+    },
+    game_line: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    minimum_players: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    maximum_players: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_by: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    updated_by: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  },
+  { timestamps: true, underscored: true }
+);
+
+module.exports = Games;

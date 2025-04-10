2abb1f9f3e8261681ba87a000f8098d1b7ac38a6
Chase Murtaugh <chasemurtaugh@gmail.com>
2025-04-09 22:12:03 -0400

    homepage clean up

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -2,56 +2,73 @@
   <div class="q-pa-md" style="max-width: 1400px">
     <div class="banner">Elysium's Newest Kindred</div>
 
-    <Suspense>
-      <div class="vamps">
-        <vampCards :currentUser="this.currentUser" />
-      </div>
-    </Suspense>
+    <div class="vamps">
+      <Suspense>
+        <template #default>
+          <vampCards :currentUser="currentUser" />
+        </template>
+
+        <template #fallback>
+          <div class="row q-col-gutter-md">
+            <q-skeleton
+              type="rect"
+              width="250px"
+              height="350px"
+              v-for="n in 3"
+              :key="n"
+            />
+          </div>
+        </template>
+      </Suspense>
+    </div>
 
     <div class="banner q-my-md">Recent Lupine Sightings</div>
-    <Suspense>
-      <div class="vamps">
-        <garouCards :currentUser="this.currentUser" />
-      </div>
-    </Suspense>
+
+    <div class="vamps">
+      <Suspense>
+        <template #default>
+          <garouCards :currentUser="currentUser" />
+        </template>
+
+        <template #fallback>
+          <div class="row q-col-gutter-md">
+            <q-skeleton
+              type="rect"
+              width="250px"
+              height="350px"
+              v-for="n in 3"
+              :key="n"
+            />
+          </div>
+        </template>
+      </Suspense>
+    </div>
 
     <div class="banner q-my-md">Beware these Hunters</div>
-    <Suspense>
-      <div class="vamps">
-        <huntCards :currentUser="this.currentUser" />
-      </div>
-    </Suspense>
+
+    <div class="vamps">
+      <Suspense>
+        <template #default>
+          <huntCards :currentUser="currentUser" />
+        </template>
+
+        <template #fallback>
+          <div class="row q-col-gutter-md">
+            <q-skeleton
+              type="rect"
+              width="250px"
+              height="350px"
+              v-for="n in 3"
+              :key="n"
+            />
+          </div>
+        </template>
+      </Suspense>
+    </div>
   </div>
 </template>
 
-<style>
-.banner {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 1em;
-  font-family: TMUnicorn;
-  text-shadow: 3px 2px 3px black;
-  font-size: 2.8em;
-}
-.vamps {
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  align-items: flex-start;
-}
-@media (max-width: 800px) {
-  .vamps {
-    flex-direction: column;
-  }
-  .banner {
-    font-size: 1.5em;
-  }
-}
-</style>
-
 <script>
-import { useQuasar } from "quasar";
-import { ref } from "vue";
 import nosImage from "../assets/images/Nosfer_logo.png";
 import vampCards from "./character_creator/vtm/vampireCards.vue";
 import huntCards from "./character_creator/hunter/hunterCards.vue";
@@ -59,94 +76,117 @@ import garouCards from "./character_creator/werewolf/garouCards.vue";
 
 import { useMeta } from "quasar";
 
-const metaData = {
-  title: "SchreckNet",
-  titleTemplate: (title) => `${title}`,
-
-  // meta tags
-  meta: {
-    keywords: {
-      name: "keywords",
-      content:
-        "vtm, character creator, vtm5e, vampire the masquerade, schrecknet, WoD, world of darkness, w5, werewolf the apocalypse, hunter the reckoning, htr",
-    },
-  },
-};
-
 export default {
   components: { vampCards, huntCards, garouCards },
 
-  setup() {
-    useMeta(metaData);
-
-    const $q = useQuasar();
-    const axios = require("axios");
-    let currentUser = ref(false);
-
-    return {
-      currentUser,
-    };
+  created() {
+    useMeta({
+      title: "SchreckNet",
+      titleTemplate: (title) => `${title}`,
+      meta: {
+        keywords: {
+          name: "keywords",
+          content:
+            "vtm, character creator, vtm5e, vampire the masquerade, schrecknet, WoD, world of darkness, w5, werewolf the apocalypse, hunter the reckoning, htr",
+        },
+      },
+    });
+  },
+  data() {
+    return { currentUser: false };
   },
   async mounted() {
-    let baseUrl = "";
-    if (window.location.href.includes("localhost")) {
-      baseUrl = "http://localhost:5000";
-    } else {
-      baseUrl = window.location.origin;
-    }
-    this.currentUser = await this.$axios
-      .get(baseUrl + "/user/currentUser", {
+    try {
+      const resp = await this.$api.get("/user/currentUser", {
         withCredentials: true,
-      })
-      .then((resp) => {
-        if (resp.data.activated != 1) {
-          this.$q.notify({
-            message:
-              "Account not yet activated! It may be closed in 60 days if not made active.",
-            color: "primary",
-            avatar: nosImage,
-            timeout: 25000,
-            actions: [
-              {
-                label: "Re-send email",
-                color: "secondary",
-                handler: async () => {
-                  let resend = await axios
-                    .post(
-                      baseUrl + "/user/resendActivation",
-                      {},
-                      {
-                        withCredentials: true,
-                      }
-                    )
-                    .then(() =>
-                      this.$q.notify({
-                        color: "green-4",
-                        textColor: "white",
-                        icon: "cloud_done",
-                        message: "Email resent!",
-                      })
-                    )
-                    .catch((err) => {
-                      console.log(err);
-                      this.$q.notify({
-                        color: "red-5",
-                        textColor: "white",
-                        icon: "warning",
-                        message: `Failed to send email, please try again later.`,
-                      });
-                    });
-                },
-              },
-              {
-                label: "Dismiss",
-                color: "white",
-              },
-            ],
-          });
-        }
-        return resp.data;
       });
+
+      this.currentUser = resp.data;
+
+      if (resp.data.activated != 1) {
+        this.$q.notify({
+          message:
+            "Account not yet activated! It may be closed in 60 days if not made active.",
+          color: "primary",
+          avatar: nosImage,
+          timeout: 25000,
+          actions: [
+            {
+              label: "Re-send email",
+              color: "secondary",
+              handler: this.resendActivation,
+            },
+            { label: "Dismiss", color: "white" },
+          ],
+        });
+      }
+    } catch (err) {
+      console.error("Failed to fetch current user", err);
+    }
+  },
+  methods: {
+    async resendActivation() {
+      try {
+        await this.$api.post(
+          "/user/resendActivation",
+          {},
+          { withCredentials: true }
+        );
+        this.$q.notify({
+          color: "green-4",
+          textColor: "white",
+          icon: "cloud_done",
+          message: "Email resent!",
+        });
+      } catch (err) {
+        console.log(err);
+        this.$q.notify({
+          color: "red-5",
+          textColor: "white",
+          icon: "warning",
+          message: `Failed to send email, please try again later.`,
+        });
+      }
+    },
   },
 };
 </script>
+
+<style>
+.vamps {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  min-height: 300px;
+}
+@media (max-width: 800px) {
+  .vamps {
+    flex-direction: column;
+    min-height: 200px;
+  }
+  .banner {
+    font-size: 1.5em;
+  }
+}
+.placeholder-card {
+  width: 250px;
+  height: 350px;
+  background: #333;
+  margin-right: 1em;
+  border-radius: 12px;
+  animation: pulse 1.5s infinite ease-in-out;
+}
+
+@keyframes pulse {
+  0% {
+    opacity: 0.6;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.6;
+  }
+}
+</style>

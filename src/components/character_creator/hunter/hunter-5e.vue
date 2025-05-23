5614b0d7f7b2ff26f457db4e70430f4508848c60
Chase Murtaugh <chasemurtaugh@gmail.com>
2025-05-22 23:41:53 -0400

    disable debug

--- src/components/character_creator/hunter/hunter-5e.vue
+++ src/components/character_creator/hunter/hunter-5e.vue
@@ -547,7 +547,7 @@ export default {
   },
   data() {
     return {
-      debug: true,
+      debug: false,
       advantagesObj: {
         merits: { advantages: [], flaws: [] },
         backgrounds: { advantages: [], flaws: [] },

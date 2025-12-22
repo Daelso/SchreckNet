04278656ee4442c1e0353bc36d6b45a71c5acefc
Daelso <11338807+Daelso@users.noreply.github.com>
2025-12-22 17:25:38 -0500

    map vtm folders

--- server/models/char_folder/CharsInFolders.js
+++ server/models/char_folder/CharsInFolders.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = require("../database");
+const db = require("../../database");
 const CharFolders = require("./CharFolders");
 
 const CharsInFolders = db.sequelize.define(
@@ -26,17 +26,33 @@ const CharsInFolders = db.sequelize.define(
     created_at: {
       type: Sequelize.DATE,
       allowNull: false,
+      defaultValue: Date.now(),
     },
     updated_at: {
       type: Sequelize.DATE,
       allowNull: false,
-    },
-    deleted: {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
+      defaultValue: Date.now(),
     },
   },
-  { timestamps: true, underscored: true }
+
+  {
+    timestamps: false,
+    underscored: true,
+    indexes: [
+      {
+        unique: true,
+        fields: ["parent_folder", "char_id"],
+      },
+    ],
+  }
 );
+
+CharsInFolders.belongsTo(CharFolders, {
+  foreignKey: "parent_folder",
+});
+
+CharFolders.hasMany(CharsInFolders, {
+  foreignKey: "parent_folder",
+});
 
 module.exports = CharsInFolders;
